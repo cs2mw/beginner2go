3f723973ed2956273ca1a56fe24581a12a9f0318
cs2mw <wcs2mw@163.com>
2018-08-21 18:22:27 +0800

    some commits

--- web/webserver.go
+++ web/webserver.go
@@ -5,24 +5,40 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"text/template"
 )
 
-func sayHelloWorld(w http.ResponseWriter, r *http.Request) {
+func sayHello(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	fmt.Println(r.Form)
 	fmt.Println("Scheme:" + r.URL.Scheme)
 	fmt.Println("Path:" + r.URL.Path)
 	fmt.Println(r.Form["param"])
 	for k, v := range r.Form {
+		fmt.Println("-------------华丽的分隔符----------------")
 		fmt.Println("Key:" + k)
 		fmt.Println("Value:" + strings.Join(v, ""))
 	}
 
-	fmt.Fprintf(w, "Hello,I'm a Go webserver!")
+	fmt.Fprintf(w, "Hello,I'm a webserver!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	m := r.Method
+	fmt.Println("method:", m)
+	if m == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
 
 func main() {
-	http.HandleFunc("/", sayHelloWorld)
+	// http.HandleFunc("/", sayHello)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal(err)
