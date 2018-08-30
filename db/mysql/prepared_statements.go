3fcf1a7730e07a6f81ec6ba622536c58b3522d7c
cs2mw <wcs2mw@163.com>
2018-08-30 16:10:25 +0800

    commit some changes

--- db/mysql/prepared_statements.go
+++ db/mysql/prepared_statements.go
@@ -15,14 +15,20 @@ func main() {
 	defer db.Close()
 
 	// Prepare statement for reading data
-	stmtOut, err := db.Prepare("SELECT Name,CountryCode,District,Population FROM city WHERE CountryCode = ?")
+	stmtCount, err := db.Prepare("SELECT count(Name) FROM city WHERE CountryCode = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmtCount.Close()
+
+	stmtOut, err := db.Prepare("SELECT Name,CountryCode,District,Population FROM city WHERE CountryCode = ? order by Name asc,District desc")
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 	defer stmtOut.Close()
 
 	var name, countryCode, district string
-	var population int // we "scan" the result in here
+	var population, rowsCount int // we "scan" the result in here
 
 	// Query the square-number of 13
 	err = stmtOut.QueryRow("AFG").Scan(&name, &countryCode, &district, &population) // WHERE number = 13
@@ -36,18 +42,26 @@ func main() {
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
-	fmt.Printf("The other city which country code is NLD is: %s,%s,%s,%d \n", name, countryCode, district, population)
+	fmt.Printf("The first city which country code is NLD is: %s,%s,%s,%d \n", name, countryCode, district, population)
 
 	// Query all results
+	err = stmtCount.QueryRow("DZA").Scan(&rowsCount)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("The city which country code is DZA contains:%d \n", rowsCount)
+
 	rows, err := stmtOut.Query("DZA")
 	if err != nil {
 		panic(err.Error())
 	}
+	count := 0
 	for rows.Next() {
 		err := rows.Scan(&name, &countryCode, &district, &population)
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("The city which country code is DZA contains: %s,%s,%s,%d \n", name, countryCode, district, population)
+		count++
+		fmt.Printf("Row %d:%s,%s,%s,%d \n", count, name, countryCode, district, population)
 	}
 }
